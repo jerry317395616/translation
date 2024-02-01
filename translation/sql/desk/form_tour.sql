a2673e9d4e0e1588388554a1e3ef758a538df600
jerry317395616 <317395616@qq.com>
2024-02-01 09:23:37 +0800

    翻译工作区引导内容

---
+++ translation/sql/desk/form_tour.sql
@@ -0,0 +1,7 @@
+update `tabForm Tour Step` set description = '这些是工作区。每个模块的工作区在一个页面上提供了有见地的信息和快捷方式。
+
+<br><br>
+
+提示：您可以根据您的需求构建自定义工作区。'
+                             ,title = '工作区列表'
+where title = 'Workspace List' and parent = 'Main Workspace Tour';
